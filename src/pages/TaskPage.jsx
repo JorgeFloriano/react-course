85231c915504958e9888bf5fba3b3dbd5c3970cb
Jorge Luis <jorgefloriano1646@gmail.com>
2025-03-16 19:52:20 -0300

    "Task screen fix 02"

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -12,23 +12,21 @@ function TaskPage() {
     const title = searchParams.get("title");
     const description = searchParams.get("description");
     return (
-        <div className="h-screen w-screen bg-slate-500 p-6">
-            <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-                <div className="w-[500px] mx-auto space-y-4">
-                    <div className="flex justify-center relative mb-6">
-                        <button
-                            onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
-                            <ChevronLeftIcon />
-                        </button>
-                        <Title>
-                        Task Details
-                        </Title>
-                    </div>
+        <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+            <div className="w-[500px] mx-auto space-y-4">
+                <div className="flex justify-center relative mb-6">
+                    <button
+                        onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
+                        <ChevronLeftIcon />
+                    </button>
+                    <Title>
+                    Task Details
+                    </Title>
+                </div>
 
-                    <div className="bg-slate-200 p-4 rounded-md">
-                    <h2 className="text-xl font-bold text-slate-600">{title}</h2>
-                    <p className="text-slate-600">{description}</p>   
-                    </div>
+                <div className="bg-slate-200 p-4 rounded-md">
+                <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+                <p className="text-slate-600">{description}</p>   
                 </div>
             </div>
         </div>
